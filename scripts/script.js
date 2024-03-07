44f0feb4af4a3ec96cd5ebfd7808666013dc6414
ArthurChenEvans <arthurchenevans@protonmail.com>
2024-03-07 23:31:14 +0000

    Added script and edited css/html slightly

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const randomNumber = Math.round(Math.random(5));
+let randomNumberDisplay = document.querySelector("h2");
+let userInput = document.querySelector("input");
+let message = document.querySelector("p");
+
+document.querySelector("button").addEventListener('click', () => {
+    console.log(userInput.value);
+    console.log(randomNumber);
+    if (userInput.value == randomNumber)
+    {
+        console.log('Same!');
+        randomNumberDisplay.textContent = randomNumber;
+        message.textContent = "Congrats you guessed the number!"
+    } else
+    {
+        console.log('Not same!');
+        if (userInput.value > randomNumber)
+        {
+            message.textContent = "Try going lower!";
+        } else if (userInput.value < randomNumber)
+        {
+            message.textContent = "Try going higher!"
+        }
+    }
+});
